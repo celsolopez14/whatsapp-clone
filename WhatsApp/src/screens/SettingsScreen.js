ee4b9cc0c633ce01e0e327fc3a983b265f90f3fe
Celso <celopez@my.bridgeport.edu>
2023-03-29 15:37:20 -0400

    Implemented amplify auth to the app

--- WhatsApp/src/screens/SettingsScreen.js
+++ WhatsApp/src/screens/SettingsScreen.js
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { Auth } from 'aws-amplify'
 
 const SettingsScreen = () => {
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
+      <Button 
+      onPress={() => Auth.signOut()}
+      title='Sign out'
+      />
     </View>
   )
 }
