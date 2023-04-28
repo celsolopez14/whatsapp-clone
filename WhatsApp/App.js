0e514c7ea723fc2b3be1dcfde394acef791c1127
Celso <celopez@my.bridgeport.edu>
2023-04-28 17:31:43 -0400

    When user signs in it creates a user on the db

--- WhatsApp/App.js
+++ WhatsApp/App.js
@@ -1,15 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,  } from 'react-native';
 import Navigation from './src/components/Navigation';
-
-import { Amplify } from 'aws-amplify';
+import * as React from 'react';
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 import { withAuthenticator } from "aws-amplify-react-native";
+import {getUser} from './src/graphql/queries';
+import {createUser} from './src/graphql/mutations';
 
 
 Amplify.configure({...awsconfig, Analytics:{disabled : true}})
 
 function App() {
+
+React.useEffect(() => {
+  const syncUser = async () => {
+
+    const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
+
+    const userData = await API.graphql(graphqlOperation(getUser, {id: authUser.attributes.sub})
+    );
+
+    if(userData.data.getUser){
+      console.log("User already exists in database.");
+      return;
+    }
+
+    const newUser = {
+      id:authUser.attributes.sub,
+      name:authUser.attributes.phone_number,
+      status:'Hey, I am using Whatsapp',
+    };
+
+    const newUserResponse = await API.graphql(graphqlOperation(createUser, {input: newUser})
+    );
+
+  };
+
+  syncUser();
+
+}, []);
 
   
   return (
