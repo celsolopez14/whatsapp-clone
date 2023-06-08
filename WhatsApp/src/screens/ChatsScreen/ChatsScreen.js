75c7835d28cc88283be19ed75473ce93f1436821
Celso <celopez@my.bridgeport.edu>
2023-06-08 16:22:13 -0400

    Sorting messages and chats by date time.

--- WhatsApp/src/screens/ChatsScreen/ChatsScreen.js
+++ WhatsApp/src/screens/ChatsScreen/ChatsScreen.js
@@ -15,7 +15,11 @@ const ChatScreen = () => {
         const fetchChatRooms = async () => {
             const authUser = await Auth.currentAuthenticatedUser();
             const response = await API.graphql(graphqlOperation(listChatRooms, {id: authUser.attributes.sub }));
-            setChatRooms(response.data.getUser.ChatRooms.items);
+
+            const rooms = response?.data?.getUser?.ChatRooms?.items || [];
+            const sortedRooms = rooms.sort((room1, room2)=> new Date(room2.chatRoom.updatedAt) - new Date(room1.chatRoom.updatedAt));
+
+            setChatRooms(sortedRooms);
         };
 
         fetchChatRooms();
